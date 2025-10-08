b9dafaea1751ccb91676dc66f9ef81afa2d00c7e
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2025-10-08 15:45:42 +0530

    cd

---
+++ esbuild.mjs
@@ -0,0 +1,12 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+  entryPoints: ['bin/www'],
+  bundle: true,
+  outfile: 'dist/server.js',
+  platform: 'node',
+  target: 'node18.12',
+  minify: true,
+  logLevel: 'info',
+  sourcemap: true,
+});
